c3838a053d22917994fbfcb5d8b5c4e1487cda4b
Hitvardhan Singh Solanki <hitvardhan19@gmail.com>
2016-07-16 19:16:21 +0000

    Challenge Destroyed!

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 var express = require('express');
 var app = express();
+var port = 8080;
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
+app.get('/getmyinfo',function(req,res){
+    var info = {
+      ipaddress : req.headers['x-forwarded-for'] ||
+       req.connection.remoteAddress ||
+       req.socket.remoteAddress ||
+       req.connection.socket.remoteAddress,
+      language : req.headers["accept-language"].split(",")[0],
+      software: req.headers["user-agent"].split("(")[1].split(")")[0]
+    };
+    res.json(info);
+});
+
+app.listen(port,function(){
+  console.log("Listening on port: " + port);
+});
